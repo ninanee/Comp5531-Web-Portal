7ec05d081ea0ee5ace113ec4d9e3cb6d20347298
Isaac <bolanle93@yahoo.com>
2021-08-11 23:05:25 -0400

    update database.php

--- database.php
+++ database.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+
+/*Your MYSQL username is xic55311
+The name of the MYSQL server is xic5531.encs.concordia.ca
+The name of the database you can use is also xic55311
+The password for your database is HuNiOl12  (case sensitive)
+You cannot change this password.
+ */
+
 $server = 'localhost:3306'; //This will be different for remote server
 $username = 'root';
 $password = '';   //check if this is correct
