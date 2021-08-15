17dc30a8c75e5c76a03ff34468795efaad17fa2a
Yun Ni <ni.yun@outlook.com>
2021-08-15 00:33:49 -0400

    add admin login page

--- index.php
+++ index.php
@@ -18,13 +18,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Portal</title>
     <link rel="stylesheet" href="main.css" type="text/css" >
-
 </head>
 <body>
   <div class = "indexContainer">
     <h1>Welcome to the Job Portal</h1>
     <h2>Please login if you already have an account, otherwise you may register</h2>
-    <ul class="listWrapper">
+    <ul>
       <li>
         <button class="loginBtn"><a href="./login.php">Login</a></button>
       </li>
@@ -35,6 +34,8 @@
         <button class="registerBtn"><a href="./registerEmployee.php">Register For Job Seekers</a></button>
       </li> 
     </ul>
+    <button class="adminLoginBtn"><a href="./adminLogin.php">Admin Login</a></button>
+    <hr>
     <div class="copyright">Created by: group xic55311</div>
   </div>
 
