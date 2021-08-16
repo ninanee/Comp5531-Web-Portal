8c9f1adc27ad5d3a40e26b45ec4744379bee5006
Yun Ni <71697803+ninanee@users.noreply.github.com>
2021-08-16 16:14:14 -0400

    Update forPassword.php

--- forPassword.php
+++ forPassword.php
@@ -5,7 +5,7 @@ session_start();
 $db_host = "localhost";
 $db_name = "test";
 $db_user = "testaccount";
-$db_pass = "niyun891029nina";
+$db_pass = "";
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 ?>
